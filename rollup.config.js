0f121cd330eb0d048a863d83ba81101b663bc2fe
nmanumr <nmanumr@gmail.com>
2021-01-14 23:58:42 +0500

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
-// import { terser } from "rollup-plugin-terser";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'src/main.js',
   external: [/@babel\/runtime/],
   output: [
-    // {file: 'build/bundle.cjs.js', format: 'cjs'},
+    {file: 'build/bundle.cjs.js', format: 'cjs', plugins: [terser()]},
     {file: 'build/bundle.js', format: 'iife', name: 'MarkupParser'},
+    {file: 'build/bundle-es.js', format: 'es', plugins: [terser()]},
     // {file: 'build/bundle.min.js', format: 'iife', name: 'MarkupParser', plugins: [terser()]},
-    // {file: 'build/bundle.umd.js', format: 'umd', name: 'MarkupParser'}
   ],
   plugins: [
     nodeResolve(),
