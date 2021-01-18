81ddb10d783c3a131058d63f983066da9101bd4c
nmanumr <nmanumr@gmail.com>
2021-01-18 15:13:43 +0500

    update dom functions

--- src/functions/dom.ts
+++ src/functions/dom.ts
@@ -1,5 +1,6 @@
 /**
- * Get inner text from HTML Element
+ * Get the text content of input element including its descendants.
+ *
  * @param node: HTML element
  * @param collapseSpaces: should collapse spaces
  * @returns inner text of the input HTML element
@@ -13,7 +14,8 @@ function text(node: HTMLElement, collapseSpaces: boolean = true): string {
 }
 
 /**
- * Get inner html from HTML Element
+ * Get the html content of descendants of input element
+ *
  * @param node: HTML element
  * @returns inner html of the input HTML element
  */
@@ -22,7 +24,8 @@ function html(node: HTMLElement): string {
 }
 
 /**
- * Get inner html from HTML Element
+ * Get the html content of input element including its descendants (also includes element itself)
+ *
  * @param node: HTML element
  * @returns outer html of the input HTML element
  */
@@ -31,7 +34,8 @@ function fullHtml(node: HTMLElement): string {
 }
 
 /**
- * Get attribute from HTML Element
+ * Get the value of an attribute of the input element
+ *
  * @param node: HTML element
  * @param attr_name: attribute name to get value of
  * @returns attribute value
@@ -75,4 +79,125 @@ function classes(node: HTMLElement): Array<string> {
  */
 function dataset(node: HTMLElement): Object {
     return node.dataset;
+}
+
+/**
+ * Get the children of the input element
+ * @param node HTML element
+ * @returns children of given element
+ */
+function children(node: HTMLElement): HTMLElement[] {
+    return Array.from(node.children) as HTMLElement[];
+}
+
+/**
+ * Get the parent of the input element
+ * @param node HTML element
+ * @returns children of given element
+ */
+// @ts-ignore
+function parent(node: HTMLElement): HTMLElement {
+    return node.parentNode as HTMLElement;
+}
+
+/**
+ * Get the ancestors of the input element up to but not including the given until selector.
+ * if no util selector provided will return all the ancestors
+ * @param node HTML element
+ * @param until A selector expression to indicate where to stop matching ancestor elements
+ * @param filter A string containing a selector expression to match elements against
+ * @returns ancestors of the input element up to the given until selector or root of tree
+ */
+function parents(node: HTMLElement, until?: string, filter?: string) {
+    const result = [];
+    const matchesSelector = node.matches || node.webkitMatchesSelector;
+
+    node = node.parentElement;
+    while (node && !(until && matchesSelector.call(node, until))) {
+        if (!filter) {
+            result.push(node);
+        } else {
+            if (matchesSelector.call(node, filter)) {
+                result.push(node);
+            }
+        }
+        node = node.parentElement;
+    }
+    return result;
+}
+
+/**
+ * Get the siblings of the input element no includes the input element
+ * @param node HTML element
+ * @returns the siblings of the input element no includes the input element
+ */
+function siblings(node: HTMLElement): HTMLElement[] {
+    return Array.from(node.parentNode.children).filter((child) => child !== node) as HTMLElement[];
+}
+
+/**
+ * Get the first element that matches the selector by testing the element itself
+ * and traversing up through its ancestors in the DOM tree.
+ * @param node HTML element
+ * @param selector A string containing a selector expression to match elements against
+ */
+function closest(node: HTMLElement, selector: string): HTMLElement {
+    return node.closest(selector);
+}
+
+/**
+ * Get the descendants of the given element matched by the given selector
+ * @param node HTML element
+ * @param selector A string containing a selector expression to match elements against
+ */
+function find(node: HTMLElement, selector: string): HTMLElement[] {
+    return Array.from(node.querySelectorAll(selector));
+}
+
+/**
+ * Get the immediately following sibling of the input element
+ * @param node HTML element
+ */
+function next(node: HTMLElement): Element {
+    return node.nextElementSibling;
+}
+
+/**
+ * Get the immediately preceding sibling of the input element
+ * @param node HTML element
+ */
+function prev(node: HTMLElement): Element {
+    return node.previousElementSibling;
+}
+
+/**
+ * Get all the following sibling of the input element. Optionally filter by given filter.
+ * @param node HTML element
+ * @param filter A string containing a selector expression to match elements against
+ */
+function nextAll(node: HTMLElement, filter?: string): HTMLElement[] {
+    const siblings = [];
+    let elm: Element = node.nextElementSibling;
+    while (elm) {
+        if (elm.nodeType === 3) continue; // ignore text nodes
+        if (!filter || elm.matches(filter)) siblings.push(elm);
+        elm = elm.nextElementSibling
+    }
+    return siblings;
+}
+
+/**
+ * Get all the preceding sibling of the input element. Optionally filter by given filter
+ * @param node HTML element
+ * @param filter A string containing a selector expression to match elements against
+ */
+function prevAll(node: HTMLElement, filter?: string): HTMLElement[] {
+    const siblings = [];
+    let elm: Element = node.previousElementSibling;
+    while (elm) {
+        if (elm.nodeType === 3) continue; // ignore text nodes
+        if (!filter || elm.matches(filter)) siblings.push(elm);
+        elm = elm.previousElementSibling
+    }
+    return siblings;
 }
